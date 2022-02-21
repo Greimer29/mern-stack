6c5dc37fa4b7590ba6ee86a3f51aa61e89bd19f9
Greimer29 <greimerabelguzman29@gmail.com>
2022-02-21 16:05:07 -0400

    Adding controllers for the api routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+    // require('./database');
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/notes', require('./routes/notes'));
+
+//settings
+app.set('port', process.env.PORT || 8080)
 
 
 module.exports = app;
